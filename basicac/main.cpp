5d378c8fc63e8c23869990c5bf46b4bb3f875363
Weston McNamara <westonfmcnamara@gmail.com>
2021-05-07 09:23:28 -0400

    removed binaries from repo

--- basicac/main.cpp
+++ basicac/main.cpp
@@ -1,6 +1,8 @@
 #include "windows.h"
 #include <iostream>
 #include "time.h"
+#include <thread>
+
 
 using namespace std;
 HWND hwnd;
@@ -13,39 +15,84 @@ int maxcps;
 int mincpsdelay;
 int maxcpsdelay;
 
+int cpsdelay1;
+int cpsdelay2;
+
+double fcpsdelay1;
+double realcpsdelay1;
+double fcpsdelay2;
+double realcpsdelay2;
+
 int RandomInt(int min, int max)
 {
-	srand(time(0));
+	srand((unsigned int)time(0));
 	return ((rand() % (int)(((max)+1) - (min))) + (min));
 }
 
-int main() {
-	cout << "min cps" << endl;
-	cin >> mincps;
-	mincpsdelay = 500 / mincps;
-	cout << "max cps" << endl;
-	cin >> maxcps;
-	maxcpsdelay = 500 / maxcps;
+void timerSleep(double seconds) { //credits to https://blat-blatnik.github.io/computerBear/making-accurate-sleep-function/ its very good function this one
+	using namespace std::chrono;
 
-	cout << "z is toggle key" << endl;
+	static HANDLE timer = CreateWaitableTimer(NULL, FALSE, NULL);
+	static double estimate = 5e-3;
+	static double mean = 5e-3;
+	static double m2 = 0;
+	static int64_t count = 1;
 
-	while (1)
+	while (seconds - estimate > 1e-7) {
+		double toWait = seconds - estimate;
+		LARGE_INTEGER due;
+		due.QuadPart = -int64_t(toWait * 1e7);
+		auto start = high_resolution_clock::now();
+		SetWaitableTimerEx(timer, &due, 0, NULL, NULL, NULL, 0);
+		WaitForSingleObject(timer, INFINITE);
+		auto end = high_resolution_clock::now();
+
+		double observed = (end - start).count() / 1e9;
+		seconds -= observed;
+
+		++count;
+		double error = observed - toWait;
+		double delta = error - mean;
+		mean += delta / count;
+		m2 += delta * (error - mean);
+		double stddev = sqrt(m2 / (count - 1));
+		estimate = mean + stddev;
+	}
+
+	// spin lock
+	auto start = high_resolution_clock::now();
+	while ((high_resolution_clock::now() - start).count() / 1e9 < seconds);
+}
+
+void ac() 
+{
+	while (TRUE)
 	{
 		hwnd = GetForegroundWindow();
 		if (toggle && GetAsyncKeyState(VK_LBUTTON))
 		{
 			if (firstclick == 1) {
-
+				cpsdelay1 = RandomInt(maxcpsdelay, mincpsdelay);
+				fcpsdelay1 = cpsdelay1;
+				realcpsdelay1 = fcpsdelay1 / 1000;
 				GetCursorPos(&pt);
-				Sleep(RandomInt(maxcpsdelay, mincpsdelay));
+				timerSleep(realcpsdelay1);
 				PostMessage(hwnd, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(pt.x, pt.y));
 				firstclick = 0;
 			}
 			else {
+				cpsdelay1 = RandomInt(maxcpsdelay, mincpsdelay);
+				fcpsdelay1 = cpsdelay1;
+				realcpsdelay1 = fcpsdelay1 / 1000;
+				cpsdelay2 = RandomInt(maxcpsdelay, mincpsdelay);
+				fcpsdelay2 = cpsdelay2;
+				realcpsdelay2 = fcpsdelay2 / 1000;
+				cout << fcpsdelay1 << endl;
+				cout << realcpsdelay1 << endl;
 				GetCursorPos(&pt);
-				Sleep(RandomInt(maxcpsdelay, mincpsdelay));
+				timerSleep(realcpsdelay1);
 				PostMessage(hwnd, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(pt.x, pt.y));
-				Sleep(RandomInt(maxcpsdelay, mincpsdelay));
+				timerSleep(realcpsdelay2);
 				PostMessage(hwnd, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(pt.x, pt.y));
 			}
 
@@ -55,7 +102,26 @@ int main() {
 			firstclick = 1;
 			Sleep(1);
 		}
+		timerSleep(0.001);
+	}
+}
 
+int main()
+{
+	cout << "min cps" << endl;
+	cin >> mincps;
+	mincpsdelay = 500 / mincps;
+	cout << "max cps" << endl;
+	cin >> maxcps;
+	maxcpsdelay = 500 / maxcps;
+
+	cout << "z is toggle key" << endl;
+
+	thread first(ac);
+	
+
+	while (1)
+	{
 		if (GetAsyncKeyState(0x5A)) {
 			toggle = !toggle;
 			cout << toggle << endl;
@@ -64,5 +130,6 @@ int main() {
 		Sleep(1);
 	}
 	return 0;
+
 }
 
